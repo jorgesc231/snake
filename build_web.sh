b2b26bcedbdb1b49ffba3911d00cd7f98144ce1c
Jorge Suarez <97117780+jorgesc231@users.noreply.github.com>
2024-09-15 01:47:12 -0300

    Fix workflow #2

--- build_web.sh
+++ build_web.sh
@@ -6,7 +6,7 @@ pushd $BUILD_DIR
 
 rm -r assets/graphics/vector
 
-# emcc -Os --use-preload-plugins ../src/main.cpp ../src/renderer.cpp ../src/assets_loader.cpp ../src/gui.cpp ../dependencies/include/imgui/imgui.cpp ../dependencies/include/imgui/imgui_draw.cpp ../dependencies/include/imgui/imgui_impl_opengl3.cpp ../dependencies/include/imgui/imgui_impl_sdl2.cpp ../dependencies/include/imgui/imgui_tables.cpp ../dependencies/include/imgui/imgui_widgets.cpp ../dependencies/include/imgui/imgui_demo.cpp -I../dependencies/include -I../dependencies/include/imgui --shell-file new_shell.html --preload-file assets -s USE_SDL=2 -s USE_SDL_MIXER=2 -s SDL2_MIXER_FORMATS=["mp3"] -s USE_SDL_IMAGE=2 -s SDL2_IMAGE_FORMATS=["png"] -s STACK_SIZE=2mb -s ALLOW_MEMORY_GROWTH -s MAX_WEBGL_VERSION=2 -o index.html
+emcc -Os --use-preload-plugins ../src/main.cpp ../src/renderer.cpp ../src/assets_loader.cpp ../src/gui.cpp ../dependencies/include/imgui/imgui.cpp ../dependencies/include/imgui/imgui_draw.cpp ../dependencies/include/imgui/imgui_impl_opengl3.cpp ../dependencies/include/imgui/imgui_impl_sdl2.cpp ../dependencies/include/imgui/imgui_tables.cpp ../dependencies/include/imgui/imgui_widgets.cpp ../dependencies/include/imgui/imgui_demo.cpp -I../dependencies/include -I../dependencies/include/imgui --shell-file new_shell.html --preload-file assets -s USE_SDL=2 -s USE_SDL_MIXER=2 -s SDL2_MIXER_FORMATS=["mp3"] -s USE_SDL_IMAGE=2 -s SDL2_IMAGE_FORMATS=["png"] -s STACK_SIZE=2mb -s ALLOW_MEMORY_GROWTH -s MAX_WEBGL_VERSION=2 -o index.html
 
 rm -r assets
 
